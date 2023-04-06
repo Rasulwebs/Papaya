3ad614010d3da3c90f2002a5fda742079ce49480
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2023-04-06 10:01:25 +0500

    card

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card from '../../components/Card/Card';
 
 const Home = () => {
     return (
         <div>
             Home
+
+
+            <Card/>
         </div>
     );
 };
